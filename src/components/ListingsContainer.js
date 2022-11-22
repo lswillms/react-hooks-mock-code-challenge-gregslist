29b2981c1e213f4f09e0fe251418177295904da3
Latoshia Williams <lswillms@yahoo.com>
2022-11-22 17:34:47 -0600

    solution

--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,11 +1,38 @@
-import React from "react";
-// import ListingCard from "./ListingCard";
+import React, { useEffect, useState } from "react";
+import ListingCard from "./ListingCard"
 
-function ListingsContainer() {
+
+function ListingsContainer( { search }) {
+
+  const [ listing, setListing ] = useState([])
+ 
+ useEffect(() => {
+  fetch("http://localhost:6001/listings")
+  .then((resp) => resp.json())
+  .then((data) => setListing(data))
+ }, []) 
+
+ function handleDeleteList(deletedItem){
+  const listAfterDelete= listing.filter((list) => list.id !== deletedItem.id)
+  setListing(listAfterDelete)
+ }
+
+ const filteredListings = listing.filter((list) => list.description.toLowerCase().includes(search.toLowerCase()))
+ console.log("filtered", filteredListings)
+
+
+
+
   return (
     <main>
       <ul className="cards">
-        {/* use the ListingCard component to display listings */}
+      {filteredListings.map((list) => (
+        <ListingCard
+        key = {list.id}
+        list = {list}
+        handleDeleteList = {handleDeleteList}
+        />
+ ))}
       </ul>
     </main>
   );
